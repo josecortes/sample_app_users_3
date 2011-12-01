e6f41fe66b97046ec048a0a6ab9a8c67f603fe0c
josecortes <jcortes178@hotmail.com>
2011-12-01 09:59:42 -0600

    created micropost file

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Micropost do
+  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should create a new micropost given valid attributes" do
+    
+  end
+  
+  it "should have a user attribute" do
+    
+  end
+  
+  it "should ahve the correct user" do
+    
+  end
+  
+end
